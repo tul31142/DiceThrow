526462f8f42d4f42522fc72c32efa2d745e6d454
Alexis Graham <tul31142@temple.edu>
2026-03-17 12:10:24 -0400

    Saved instance state working!

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -10,9 +10,11 @@ import kotlin.random.Random
 
 class DieFragment : Fragment() {
 
-    val DIESIDE = "sidenumber"
+    private val DIESIDE = "sidenumber"
+    private val ROLL_KEY = "current_role"
 
     lateinit var dieTextView: TextView
+    var currentRoll = 1
 
     var dieSides: Int = 6
 
@@ -22,6 +24,10 @@ class DieFragment : Fragment() {
             it.getInt(DIESIDE).run {
                 dieSides = this
             }
+        }
+
+        savedInstanceState?.run{
+            currentRoll = getInt(ROLL_KEY)
         }
     }
 
@@ -37,10 +43,21 @@ class DieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        throwDie()
+
+        if (currentRoll == 0)
+            throwDie()
+        else
+            dieTextView.text = currentRoll.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(ROLL_KEY, currentRoll)
     }
 
     fun throwDie() {
-        dieTextView.text = Random.nextInt(1,dieSides+1).toString()
+        currentRoll = Random.nextInt(1,dieSides+1)
+        dieTextView.text = currentRoll.toString()
     }
 }
